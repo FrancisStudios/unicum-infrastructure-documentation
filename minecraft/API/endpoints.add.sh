72ee0669805a5d882dec11bf7fa6c6eb8d5d853a
unknown <vlfmap@gmail.com>
2023-08-09 10:13:44 +0200

    added endpoint doc generator

---
+++ minecraft/API/endpoints.add.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ENDPOINTS_DOCUMENT="endpoints.md"
+
+#FIELDS IN TABLE - MODIFY HERE IF WE CHANGE FIELDS
+declare -a TABLE_FIELDS=(
+    "PHYSICAL TARGET" 
+    "ENDPOINT"
+    "METHOD" 
+    "REQUEST" 
+    "RESPONSE"
+    "WHAT DOES IT DO?"
+)
+
+echo -e "\e[1;31m\e[47mDocument UNICUM Minecraft API Endpoint\e[0m"
+
+#INSERT NEW LINE TO ENDPOINTS
+echo "" >> $ENDPOINTS_DOCUMENT
+
+for FIELD in "${TABLE_FIELDS[@]}"; do 
+    read -p "$FIELD: " ANS
+    echo -e "\e[0;33m$ANS\e[0m is written to table"
+    echo -n "| $ANS " >> $ENDPOINTS_DOCUMENT
+done
+
+#CLOSE TABLE
+echo -n "|" >> $ENDPOINTS_DOCUMENT
